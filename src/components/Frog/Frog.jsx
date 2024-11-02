8634b6c399eb75f0e85dbc212290bd2dddd62202
Dominik Lewczyński <dominiklew200060@gmail.com>
2024-11-02 19:06:19 +0100

    Update Frog

--- src/components/Frog/Frog.jsx
+++ src/components/Frog/Frog.jsx
@@ -1,13 +1,18 @@
+// src/components/Frog/Frog.jsx
 import './Frog.css';
 
-const Frog = ({ type, isSelected, onSelect }) => {
+const Frog = ({ id, type, isSelected, onSelect, characteristics = [], showTooltip = true }) => {
     return (
-        <label className={`frog ${type} ${isSelected ? 'selected' : ''}`}>
+        <label
+            className={`frog ${type} ${isSelected ? 'selected' : ''}`}
+            title={showTooltip ? `ID: ${id}\nType: ${type}\nCharacteristics: ${characteristics.join(', ')}` : ''} // Dodanie warunku dla title
+        >
             <input
                 type="checkbox"
                 checked={isSelected}
                 onChange={onSelect}
             />
+            <span className={`frog-indicator ${type}`}></span>
         </label>
     );
 };
