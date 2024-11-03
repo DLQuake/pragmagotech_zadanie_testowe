3849687cc1672b93767f381da2153201d9517bf2
Dominik Lewczyński <dominiklew200060@gmail.com>
2024-11-03 18:33:04 +0100

    Delete Controls.css and add class is-centered to buttons

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -1,10 +1,8 @@
-import './Controls.css'
-
 const Controls = ({ onJump, onReproduce }) => {
     return (
         <div className="mt-5">
             <h3 className="title is-size-4">Actions</h3>
-            <div className="buttons">
+            <div className="buttons is-centered">
                 <button className="button is-primary" onClick={onJump}>Jump</button>
                 <button className="button is-info" onClick={onReproduce}>Reproduce</button>
             </div>
