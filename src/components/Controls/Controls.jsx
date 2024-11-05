17cd16752fcd1b7e186fad0a41d770f23ac08afa
Dominik Lewczyński <dominiklew200060@gmail.com>
2024-11-05 17:52:19 +0100

    Update Component Controls

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -1,8 +1,10 @@
+import "./Controls.css"
+
 const Controls = ({ onJump, onReproduce }) => {
     return (
         <div className="mt-5">
             <h3 className="title is-size-4">Actions</h3>
-            <div className="buttons is-centered">
+            <div className="buttons">
                 <button className="button is-primary" onClick={onJump}>Jump</button>
                 <button className="button is-info" onClick={onReproduce}>Reproduce</button>
             </div>
